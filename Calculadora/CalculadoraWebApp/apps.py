9aef2c355a2032cb022e7f8e39389caf71ad4f44
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-11-24 18:10:20 +0100

    añadido el código para conectarlo a Airtable

--- Calculadora/CalculadoraWebApp/apps.py
+++ Calculadora/CalculadoraWebApp/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CalculadorawebappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "CalculadoraWebApp"
+
--- Calculadora/CalculadoraWebApp/views.py
+++ Calculadora/CalculadoraWebApp/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Operacion
+import json
 
 def index(request):
     return render(request, "CalculadoraWebApp/index.html")
+
+
+
+def guardar_operacion(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        operacion = data.get('operacion')
+        resultado = data.get('resultado')
+        Operacion.objects.create(operacion=operacion, resultado=resultado)
+        return JsonResponse({'message': 'Operación guardada correctamente'}, status=201)
+    else:
+        return JsonResponse({'error': 'Método no permitido'}, status=405)
