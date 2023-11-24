0ac190adc3df19eadf6a8d2a41482ca9987443ab
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2023-11-24 21:29:06 +0100

    Actualizar settings.py

--- Calculadora/Calculadora/settings.py
+++ Calculadora/Calculadora/settings.py
@@ -127,8 +127,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 
-base_key = 'app2knqNuI7aBzf87'  # Reemplaza con el ID de tu base de datos en Airtable
+base_key = ''
 table_name = 'Prueba'
-api_key = 'pat67idihhcENInZt.df763f97e58ef668aad585463681615625e6aebdf4c7a284cb4a0269de693f01'
+api_key = ''
 
 airtable = Airtable(base_key, table_name, api_key)
