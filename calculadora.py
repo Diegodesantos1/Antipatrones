dbebf22e503ca761de57df8955c99e86ac799650
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-11-24 17:05:13 +0100

    montado django básico y calculadora en TK

--- calculadora.py
+++ calculadora.py
@@ -5,58 +5,113 @@ class Calculadora(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.master.title("Calculadora")
         self.pack()
         self.crear_widgets()
+        self.operacion_actual = None
+        self.valor_anterior = None
+        self.limpiar_pantalla = False
 
-    def sumar(self):
-        self.resultado["text"] = float(self.num1.get()) + float(self.num2.get())
-
-    def restar(self):
-        self.resultado["text"] = float(self.num1.get()) - float(self.num2.get())
-
-    def multiplicar(self):
-        self.resultado["text"] = float(self.num1.get()) * float(self.num2.get())
-
-    def dividir(self):
-        if float(self.num2.get()) != 0:
-            self.resultado["text"] = float(self.num1.get()) / float(self.num2.get())
+    def agregar_numero(self, numero):
+        entry_text = self.num_display.get()
+        if self.limpiar_pantalla:
+            self.num_display.set(numero)
+            self.limpiar_pantalla = False
         else:
-            self.resultado["text"] = "No se puede dividir entre cero."
+            if entry_text == "0" or entry_text.startswith("No se puede"):
+                self.num_display.set(numero)
+            else:
+                self.num_display.set(entry_text + numero)
+
+    def limpiar_display(self):
+        self.num_display.set("0")
+
+    def realizar_operacion(self, operacion):
+        valor_actual = float(self.num_display.get())
+        if self.valor_anterior is not None:
+            if self.operacion_actual == '+':
+                self.valor_anterior += valor_actual
+            elif self.operacion_actual == '-':
+                self.valor_anterior -= valor_actual
+            elif self.operacion_actual == '*':
+                self.valor_anterior *= valor_actual
+            elif self.operacion_actual == '/':
+                if valor_actual != 0:
+                    self.valor_anterior /= valor_actual
+                else:
+                    self.num_display.set("No se puede dividir entre cero")
+                    return
+
+        self.operacion_actual = operacion
+        self.valor_anterior = float(self.num_display.get())
+        self.limpiar_pantalla = True
+
+    def calcular_resultado(self):
+        if self.operacion_actual and self.valor_anterior is not None:
+            valor_actual = float(self.num_display.get())
+            if self.operacion_actual == '+':
+                valor_actual = self.valor_anterior + valor_actual
+            elif self.operacion_actual == '-':
+                valor_actual = self.valor_anterior - valor_actual
+            elif self.operacion_actual == '*':
+                valor_actual = self.valor_anterior * valor_actual
+            elif self.operacion_actual == '/':
+                if valor_actual != 0:
+                    valor_actual = self.valor_anterior / valor_actual
+                else:
+                    self.num_display.set("No se puede dividir entre cero")
+                    return
+
+            self.num_display.set(str(valor_actual))
+            self.valor_anterior = None
+            self.operacion_actual = None
+            self.limpiar_pantalla = True
 
     def crear_widgets(self):
-        self.num1 = tk.Entry(self)
-        self.num1.pack(side="left")
+        self.num_display = tk.StringVar()
+        self.num_display.set("0")
 
-        self.num2 = tk.Entry(self)
-        self.num2.pack(side="left")
+        display_frame = tk.Frame(self)
+        display_frame.pack()
 
-        self.suma = tk.Button(self)
-        self.suma["text"] = "+"
-        self.suma["command"] = self.sumar
-        self.suma.pack(side="left")
+        display_entry = tk.Entry(display_frame, textvariable=self.num_display, font=("Arial", 12), state='disabled')
+        display_entry.pack(side="left", padx=5, pady=5)
 
-        self.resta = tk.Button(self)
-        self.resta["text"] = "-"
-        self.resta["command"] = self.restar
-        self.resta.pack(side="left")
+        num_buttons_frame = tk.Frame(self)
+        num_buttons_frame.pack()
 
-        self.multiplicacion = tk.Button(self)
-        self.multiplicacion["text"] = "*"
-        self.multiplicacion["command"] = self.multiplicar
-        self.multiplicacion.pack(side="left")
+        buttons_layout = [
+            '7', '8', '9',
+            '4', '5', '6',
+            '1', '2', '3',
+            '.', '0', 'C'
+        ]
 
-        self.division = tk.Button(self)
-        self.division["text"] = "/"
-        self.division["command"] = self.dividir
-        self.division.pack(side="left")
+        for i, num in enumerate(buttons_layout):
+            if num != 'C':
+                button = tk.Button(num_buttons_frame, text=num, width=5, height=2, font=("Arial", 12),
+                                    command=lambda n=num: self.agregar_numero(n))
+            else:
+                button = tk.Button(num_buttons_frame, text=num, width=5, height=2, font=("Arial", 12),
+                                    command=self.limpiar_display)
 
-        self.resultado = tk.Label(self)
-        self.resultado["text"] = "Resultado"
-        self.resultado.pack(side="left")
+            button.grid(row=i // 3, column=i % 3, padx=5, pady=5)
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
+        operation_frame = tk.Frame(self)
+        operation_frame.pack()
+
+        operations = ['+', '-', '/', '=', '*']  # Reordenados
+        for i, op in enumerate(operations):
+            button = tk.Button(operation_frame, text=op, width=5, height=2, font=("Arial", 12),
+                                command=lambda symbol=op: self.realizar_operacion(symbol) if symbol != '=' else self.calcular_resultado())
+            button.grid(row=0, column=i, padx=5, pady=5)
+
+        quit_frame = tk.Frame(self)
+        quit_frame.pack()
+
+        self.quit = tk.Button(quit_frame, text="QUIT", fg="red", font=("Arial", 12),
                                 command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.quit.pack(padx=5, pady=5)
 
 root = tk.Tk()
 app = Calculadora(master=root)
